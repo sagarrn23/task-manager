536cf6d8efc5fbb8df917044b7d298aea18fad09
Sagar Chaudhari <sagarchaudhari250@gmail.com>
2022-01-17 20:58:24 +0530

    Checking router path

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import { Board, Home } from './container';
 
+const prodBasePath = import.meta.env.PROD
+	? 'https://sagarrn23.github.io/task-manager/'
+	: '/';
+
 const Router = () => {
 	return (
 		<Switch>
-			<Route path="/board" component={Board} />
-			<Route exact path="/" component={Home} />
+			<Route path={prodBasePath + 'board'} component={Board} />
+			<Route exact path={prodBasePath} component={Home} />
 			<Route path="*">
-				<Redirect to="/" />
+				<Redirect to={prodBasePath} />
 			</Route>
 		</Switch>
 	);
